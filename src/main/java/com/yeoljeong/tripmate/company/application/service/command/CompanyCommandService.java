4872ca34d13432d929d76c03e43618abe9fe1710
yeojin-c <orlina2001@naver.com>
2026-04-25 10:58:14 +0900

    feat: implement CompanyCommandService

---
+++ src/main/java/com/yeoljeong/tripmate/company/application/service/command/CompanyCommandService.java
@@ -0,0 +1,58 @@
+package com.yeoljeong.tripmate.company.application.service.command;
+
+import com.yeoljeong.tripmate.company.domain.entity.Company;
+import com.yeoljeong.tripmate.company.domain.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyCommandService {
+
+  private final CompanyRepository companyRepository;
+
+  //업체 생성
+  public Company createCompany(
+      String name,
+      String businessNumber,
+      String description,
+      String email,
+      String phone
+  ) {
+
+    validateDuplicateCompany(businessNumber);
+
+    Company company = createCompanyEntity(
+        name, businessNumber, description, email, phone
+    );
+
+    return companyRepository.save(company);
+  }
+
+
+  //==메서드==
+
+  //사업자등록번호 중복 검증
+  private void validateDuplicateCompany(String businessNumber) {
+    if (companyRepository.existsByBusinessNumber(businessNumber)) {
+      throw new IllegalArgumentException("이미 등록된 사업자입니다.");
+    }
+  }
+
+  //Company 엔티티 생성
+  private Company createCompanyEntity(
+      String name,
+      String businessNumber,
+      String description,
+      String email,
+      String phone
+  ) {
+    return Company.builder()
+        .name(name)
+        .businessNumber(businessNumber)
+        .description(description)
+        .contactEmail(email)
+        .contactPhone(phone)
+        .build();
+  }
+}
