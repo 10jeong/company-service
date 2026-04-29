df8d0253759105627c7c5560ffd140c42a6e3cc8
yeojin-c <orlina2001@naver.com>
2026-04-30 01:48:03 +0900

    feat: 해당 날짜 상품 조회 API 추가 및 관련 서비스, DTO 구현

---
+++ src/main/java/com/yeoljeong/tripmate/product/application/service/query/ProductSearchService.java
@@ -0,0 +1,8 @@
+package com.yeoljeong.tripmate.product.application.service.query;
+
+
+//날짜 기준으로 판매 가능한 상품 조회
+//ex ) 5월 28일 -> 박물관 입장권 , 성당 입장권 , 낙타 투어  ..
+public class ProductSearchService {
+
+}
