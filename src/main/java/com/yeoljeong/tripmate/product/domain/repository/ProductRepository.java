5335ebffba30589cff99db581286d6908440a527
yeojin-c <orlina2001@naver.com>
2026-05-02 12:13:25 +0900

    docs: Product Repository  주석 및 메서드 역할 설명 추가

--- src/main/java/com/yeoljeong/tripmate/product/domain/repository/ProductRepository.java
+++ src/main/java/com/yeoljeong/tripmate/product/domain/repository/ProductRepository.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.Slice;
 
 public interface ProductRepository {
 
+  // 상품 저장
   Product save(Product product);
 
+  // 상품 단건 조회
   Optional<Product> findById(UUID id);
 
+  // 상품 목록 조회
   Slice<Product> findAll(Pageable pageable);
 }
