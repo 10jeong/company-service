71a882874da62b603a2d9672bb50fc3389a5d353
yeojin-c <orlina2001@naver.com>
2026-05-14 22:24:26 +0900

    feat: 일정 내부 통신용 스케줄 ID 리스트 기반 상품 스케줄 다건 조회 내부 API 추가

--- src/main/java/com/yeoljeong/tripmate/product/presentation/controller/internal/ProductFeignClientController.java
+++ src/main/java/com/yeoljeong/tripmate/product/presentation/controller/internal/ProductFeignClientController.java
@@ -3,7 +3,9 @@ package com.yeoljeong.tripmate.product.presentation.controller.internal;
 import com.yeoljeong.tripmate.product.application.service.query.ProductQueryService;
 import com.yeoljeong.tripmate.product.application.service.query.ProductScheduleQueryService;
 import com.yeoljeong.tripmate.product.presentation.dto.response.ProductScheduleInfoResponse;
+import com.yeoljeong.tripmate.product.presentation.dto.response.ProductScheduleSimpleResponse;
 import com.yeoljeong.tripmate.product.presentation.dto.response.WithdrawalCheckResponse;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +30,17 @@ public class ProductFeignClientController {
     return ProductScheduleInfoResponse.from(
         productScheduleQueryService.getSchedule(scheduleId)
     );
+  }
+
+  //일정에게 넘겨주는 리스트 정보
+  @GetMapping("/schedules")
+  public List<ProductScheduleSimpleResponse> getSchedules(
+      @RequestParam List<UUID> scheduleIds
+  ) {
+    return productScheduleQueryService.getSchedules(scheduleIds)
+        .stream()
+        .map(ProductScheduleSimpleResponse::from)
+        .toList();
   }
 
   // 주문시 사용 하는 상품, 스케줄 정보
